ab2719c4760804c62607d2605ed1c6786fdd6039
Nora Koirala (SHE/HER) <norakoirala@microsoft.com>
2023-05-10 17:17:28 -0700

    a

--- main/cmds.go
+++ main/cmds.go
@@ -496,7 +496,8 @@ func deleteScriptsAndSettingsExceptMostRecent(dataDir string, downloadDir string
 	downloadParent := filepath.Join(dataDir, downloadDir)
 	regex1 := extName + "\\d+.settings"
 	regex2 := extName + "%d.settings"
-	ctx.Log("event", "clearing settings and script files except most recent seq num", regex1, regex2)
+	mostRecentRuntimeSetting := fmt.Sprintf(regex2, strconv.Itoa(seqNum))
+	ctx.Log("event", "clearing settings and script files except most recent seq num", regex1, mostRecentRuntimeSetting)
 	err := utils.TryClearExtensionScriptsDirectoriesAndSettingsFilesExceptMostRecent(downloadParent, h.HandlerEnvironment.ConfigFolder, "", uint64(seqNum), regex1, regex2)
 	if err != nil {
 		ctx.Log("event", "could not clear settings and script files")
